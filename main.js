1627da1e2b45ddeb153abbd8557a1687fbb0f194
Matheus <mtsmelo95@gmail.com>
2023-06-01 17:25:47 -0300

    landing page

---
+++ main.js
@@ -0,0 +1,33 @@
+function getProject(){
+    const urlGitHub = "https://api.github.com/users/mtsmelo95/repos";
+    var loadingElement = document.getElementById("loading");
+
+    fetch(urlGitHub, {
+        method: "GET"
+    })
+        .then((response) => response.json())
+        .then((response) => {
+            loadingElement.style.display = "none";
+            console.log(response)
+            showProjects(response)
+        })
+        .catch((e) =>{
+            console.log(e);
+        })
+}
+
+function showProjects(data){
+    var listElement = document.getElementById("my-projects-list");
+
+    for(let i=0; i<data.length; i++){
+        let a = document.createElement("a")
+        a.href = data[i]['clone_url']
+        a.target = "_blank"
+        a.title = data[i]['description']
+        let linkText = document.createTextNode(data[i]['name'])
+        a.appendChild(linkText)
+        listElement.appendChild(a)
+    }
+}
+
+getProject()
